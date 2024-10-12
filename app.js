b7d977d634292186f7223bdcb3096cfd7ace737e
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-12 19:30:33 +0300

    .

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname });
-  res.sendFile('script.js', { root: __dirname });
   res.sendFile('style.css', { root: __dirname });
-
 })
 
 app.listen(PORT)
