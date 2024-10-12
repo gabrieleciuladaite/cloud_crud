41c9b4059894772958b7a3e84a71af4c5aee756d
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-12 19:58:42 +0300

    connection

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ app.get('/', function (req, res) {
 
 app.listen(PORT)
     console.log(`Listening on port ${ PORT }`)
+
+const mongoose = require('mongoose');
+
+const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
+const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
+    
+mongoose.connect(MONGODB_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser:true,
+    useCreateIndex: true
+  })
+  .then(db => console.log('DB Connection: true'))
+  .catch(err => console.log(err))
