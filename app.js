5f271cf92af720fd09ddfe622afde92709170628
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-12 20:19:15 +0300

    .

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.listen(PORT)
 const mongoose = require('mongoose');
 
 const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
+console.log(WEB_APP_NOTES_MONGODB_HOST)
+console.log(WEB_APP_NOTES_MONGODB_DATABASE)
 const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
     
 mongoose.connect(MONGODB_CONNECTION, {
