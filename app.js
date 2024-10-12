a913dd46d7394ef9babb3f9eb9a6d3a61da26fea
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-12 19:16:30 +0300

    .

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile('index.html', { root: __dirname });
 })
 
 app.listen(PORT)
