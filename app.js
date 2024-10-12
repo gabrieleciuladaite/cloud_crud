f1852420d31c837be6c49cf7449a3308ef3b2a9d
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-12 18:56:09 +0300

    change

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello from Group 2!!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello from Group 2!!')
+// })
 
 app.listen(PORT)
     console.log(`Listening on port ${ PORT }`)
