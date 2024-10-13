b2911c6e475629b24df2b2f89f3119c3d175294d
gabrieleciuladaite <gabriele.ciuladaite@gmail.com>
2024-10-13 12:10:24 +0300

    .

--- app.js
+++ app.js
@@ -5,25 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-
-const mongoose = require('mongoose');
-
-const {WEB_APP_NOTES_MONGODB_HOST,WEB_APP_NOTES_MONGODB_DATABASE} = process.env;
-console.log(WEB_APP_NOTES_MONGODB_HOST)
-console.log(WEB_APP_NOTES_MONGODB_DATABASE)
-const MONGODB_CONNECTION = `mongodb://${WEB_APP_NOTES_MONGODB_HOST}/${WEB_APP_NOTES_MONGODB_DATABASE}`;
-    
-mongoose.connect(MONGODB_CONNECTION, {
-    useUnifiedTopology: true,
-    useNewUrlParser:true,
-    useCreateIndex: true
-  })
-  .then(db => console.log('DB Connection: true'))
-  .catch(err => console.log(err))
-
-
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: __dirname }, db);
+  res.sendFile('index.html', { root: __dirname });
 })
 
 app.listen(PORT)
